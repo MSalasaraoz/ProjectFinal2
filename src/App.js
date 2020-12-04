591d631a379326e563dffc4a7a91a482cb738f47
Jimena Bulacio <jbulacio@argentilemon.net>
2020-12-04 13:20:59 -0300

    elaboracion formulario nuevo usuario con card de bienvenida

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NewUser from './Components/NewUserComponents/NewUser';
+import AdminNewUser from './Components/Admin/AdminNewUser';
+import Inicio from './Components/Inicio';
+import Nosotros from './Components/Nosotros';
+import {ButtonGroup, Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <ButtonGroup arial-label="Basic example">
+            <Link to="/">
+              <Button variant="danger">Inicio</Button>
+            </Link>
+            <Link to="/nosotros">
+              <Button variant="danger">Nosotros</Button>
+            </Link>
+            <Link to="/newuser">
+              <Button variant="danger">Nuevo Usuario</Button>
+            </Link>
+            <Link to="/admin">
+              <Button variant="danger">Administrador</Button>
+            </Link>
+          </ButtonGroup>
+        </div>
+        <Switch>
+          <Route path="/" exact>
+            <Inicio />
+          </Route>
+          <Route path="/nosotros">
+            <Nosotros />
+          </Route>
+          <Route path="/newuser">
+            <NewUser />
+          </Route>
+          <Route path="/admin">
+            <AdminNewUser />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
