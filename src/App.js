496386e3dec12960ec0d9f6059401112346f36f8
Gabriel Alabí <ivangabrielalabi@gmail.com>
2020-12-13 00:15:23 -0300

    arregle algo de los scripta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import CardFamous from './components/CardFamous'
+import CardFamous from './Components/CardFamous'
 function App() {
   return (
     
